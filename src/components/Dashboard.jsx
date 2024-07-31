ab242f9faa82983b2c3954c1dbb8aa0a8c048f2a
snehakore54 <151037050+snehakore54@users.noreply.github.com>
2024-07-31 13:24:43 +0530

    Update dependencies and fix configuration

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,7 +4,7 @@ import "../App.css";
 function Dashboard() {
   const userData = JSON.parse(localStorage.getItem("userData"));
   if (!userData) {
-    return <p>Please login to view the dashboard.</p>;
+    return <p style={{color:'white'}}>Please login to view the dashboard.</p>;
   }
 
   return (
@@ -12,7 +12,7 @@ function Dashboard() {
       <h2>Welcome, {userData.user_firstname}!</h2>
       <p>Email: {userData.user_email}</p>
       <p>Phone: {userData.user_phone}</p>
-      {/* Add other dashboard elements as needed */}
+     
     </div>
   );
 }
